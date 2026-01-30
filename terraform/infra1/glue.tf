a664a14787eb6bea5c48c81347dade198dceb9da
Sahilpawar77 <pawarsahil0763@gmail.com>
2026-01-30 20:35:32 +0530

    Update glue.tf

--- terraform/infra1/glue.tf
+++ terraform/infra1/glue.tf
@@ -1,19 +1,18 @@
 resource "aws_glue_job" "pgyr_job" {
-  name     = "ter-bronze-to-silver-job"
+  name     = var.glue_job_name
   role_arn = aws_iam_role.glue_role.arn
 
   command {
     name            = "glueetl"
-    script_location = "s3://ter-bronze-data-sahil/scripts/bronze_to_silver.py"
+    script_location = var.glue_script_s3_path
     python_version  = "3"
   }
 
-  glue_version      = "4.0"
-  worker_type       = "G.1X"
-  number_of_workers = 2
+  glue_version      = var.glue_version
+  worker_type       = var.worker_type
+  number_of_workers = var.number_of_workers
 
   default_arguments = {
     "--job-language" = "python"
   }
 }
-
