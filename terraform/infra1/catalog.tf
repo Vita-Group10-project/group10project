45d6e3dbc4e1819e467171326dff5615b8cd2276
rathorepuneet99 <rathorepuneet99@gmail.com>
2026-02-02 11:56:05 +0530

    Update catalog.tf

--- terraform/infra1/catalog.tf
+++ terraform/infra1/catalog.tf
@@ -1,9 +1,3 @@
-terraform import aws_s3_bucket.bronze ter-raw-zone-sahil
-terraform import aws_s3_bucket.silver ter-enriched-zone-sahil
-terraform import aws_s3_bucket.gold ter-reorting-zone-sahil
-terraform import aws_glue_crawler.silver_crawler ter-silver-crawler
-terraform import aws_glue_catalog_database.silver_db ter_silver_db
-
 ############################
 # GLUE CRAWLER
 ############################
