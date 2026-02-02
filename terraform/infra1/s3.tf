3f1d911298d20c835bded758fe0414f63d74f091
rathorepuneet99 <rathorepuneet99@gmail.com>
2026-02-02 11:51:59 +0530

    Update s3.tf

--- terraform/infra1/s3.tf
+++ terraform/infra1/s3.tf
@@ -1,16 +1,47 @@
+terraform import aws_s3_bucket.bronze ter-raw-zone-sahil
+terraform import aws_s3_bucket.silver ter-enriched-zone-sahil
+terraform import aws_s3_bucket.gold ter-reorting-zone-sahil
+terraform import aws_glue_crawler.silver_crawler ter-silver-crawler
+terraform import aws_glue_catalog_database.silver_db ter_silver_db
+
+############################
+# BRONZE BUCKET
+############################
 resource "aws_s3_bucket" "bronze" {
   bucket = "ter-raw-zone-sahil"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+############################
+# SILVER BUCKET
+############################
 resource "aws_s3_bucket" "silver" {
   bucket = "ter-enriched-zone-sahil"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+############################
+# GOLD BUCKET
+############################
 resource "aws_s3_bucket" "gold" {
   bucket = "ter-reorting-zone-sahil"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-resource "aws_s3_bucket" "raw" {
-  bucket = "ter-dump-sahil"
+############################
+# CRAWLER FOLDER (IMPORTANT)
+############################
+resource "aws_s3_object" "crawler_prefix" {
+  bucket  = aws_s3_bucket.silver.bucket
+  key     = "output/"
+  content = ""
 }
-
