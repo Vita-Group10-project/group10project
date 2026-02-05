e5358f84a9731f264f6de97eeef4174b3e574ed0
govinddhande27 <govindpatil2712@gmail.com>
2026-02-05 17:15:27 +0530

    Delete work_010 directory

--- work_010/terraform/crawler.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_glue_crawler" "clean_crawler" {
-  name          = "clean-data-crawler"
-  role          = aws_iam_role.glue_role.arn
-  database_name = "clean_db"
-
-  s3_target {
-    path = "s3://my-project-clean-data/output/"
-  }
-}
--- work_010/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_athena_workgroup" "analytics" {
-  name = "analytics-wg"
-
-  configuration {
-    result_configuration {
-      output_location = "s3://my-project-athena-results/"
-    }
-  }
-}
