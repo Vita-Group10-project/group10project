f405747a57882e9262f1ded20a21d9e7a52cc3b9
Sahilpawar77 <pawarsahil0763@gmail.com>
2026-02-05 13:59:25 +0530

    Update s3.tf

--- terraform/infra1/s3.tf
+++ terraform/infra1/s3.tf
@@ -1,41 +1,11 @@
-############################
-# BRONZE BUCKET
-############################
-resource "aws_s3_bucket" "bronze" {
-  bucket = "ter-raw-zone-sahil"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+data "aws_s3_bucket" "bronze" {
+  bucket = "cms-open-payment-raw-data "
 }
 
-############################
-# SILVER BUCKET
-############################
-resource "aws_s3_bucket" "silver" {
-  bucket = "ter-enriched-zone-sahil"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+data "aws_s3_bucket" "silver" {
+  bucket = "enriched-zone-00"
 }
 
-############################
-# GOLD BUCKET
-############################
-resource "aws_s3_bucket" "gold" {
-  bucket = "ter-reorting-zone-sahil"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-############################
-# CRAWLER FOLDER (IMPORTANT)
-############################
-resource "aws_s3_object" "crawler_prefix" {
-  bucket  = aws_s3_bucket.silver.bucket
-  key     = "output/"
-  content = ""
+data "aws_s3_bucket" "gold" {
+  bucket = "reporting-zone-00"
 }
